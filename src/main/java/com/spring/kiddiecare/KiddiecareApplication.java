73885d66667694846983bb3de3bee3ef231c3830
gitjae <dlrbwo2022@gmail.com>
2023-08-07 11:16:42 +0900

    Initial commit

---
+++ src/main/java/com/spring/kiddiecare/KiddiecareApplication.java
@@ -0,0 +1,13 @@
+package com.spring.kiddiecare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiddiecareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiddiecareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/kiddiecare/KiddiecareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.kiddiecare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiddiecareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
