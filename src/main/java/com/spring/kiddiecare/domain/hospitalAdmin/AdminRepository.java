90b23f83baa0f2eccafd8354e341d67f94a4eac6
heejae101 <heejae0629@naver.com>
2023-08-08 18:07:09 +0900

    add 1 added login.css 3 modified Admin #2-heejae

--- src/main/java/com/spring/kiddiecare/domain/hospitalAdmin/AdminRepository.java
+++ src/main/java/com/spring/kiddiecare/domain/hospitalAdmin/AdminRepository.java
@@ -3,9 +3,7 @@ package com.spring.kiddiecare.domain.hospitalAdmin;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface AdminRespository extends JpaRepository<Admin,Long> {
+public interface AdminRepository extends JpaRepository<Admin,Long> {
 
     public Admin findByAdminIdAndAdminPw(String adminId, String AdminPw);
 
