f19e5cdb49c464c49d3747b97af251a52a1d1bae
gitjae <dlrbwo2022@gmail.com>
2023-08-08 11:05:27 +0900

    test case

---
+++ src/test/java/com/spring/kiddiecare/service/ExternalApiServiceTest.java
@@ -0,0 +1,21 @@
+package com.spring.kiddiecare.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.ValueOperations;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ExternalApiServiceTest {
+
+
+    private ValueOperations<String, String> valueOps;
+    @Test
+    public void fetchData() {
+    }
+
+    @Test
+    public void fetchData2() {
+    }
+}
