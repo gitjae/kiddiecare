d6d0bc243be4e3630977116ff84b1ee5e619d9ea
han_heesoo <juntu09@gmail.com>
2023-08-16 22:04:42 +0900

    Modified) hospital-detail.js/예약시간 미선택 시 얼럿 #4-heesoo

---
+++ src/main/java/com/spring/kiddiecare/controller/TimeSlotsViewController.java
@@ -0,0 +1,10 @@
+package com.spring.kiddiecare.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes({"log"})
+public class TimeSlotsViewController {
+    // 실수로 지운 파일 다시 살림.. -희수
+}
